dd985182be5e63808485b2feb95bddf2d80a7723
Tony Wilsman <tony@ideabankmarketing.com>
2011-07-01 15:20:08 -0500

    Starting the Dev branch

---
+++ dbedit.config.php
@@ -0,0 +1,4 @@
+<?php
+$core_path = $modx->getOption('dbedit.core_path',null,$modx->getOption('core_path'));
+$assets_url = $modx->getOption('dbedit.assets_url',null,$modx->getOption('assets_url'));
+?>
